0a488e099a4be639015adac24e96144e004deb35
borok-cloud <54220437+borok-cloud@users.noreply.github.com>
2025-03-09 15:46:34 -0400

    Update DemoController.java
    added REST endpoints for both authorization_code and client_credentials grant type

--- DemoController.java
+++ DemoController.java
@@ -3,11 +3,17 @@ package com.mars.demoOkata;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 //@PreAuthorize("isAuthenticated()")
 public class DemoController {
 
@@ -18,8 +24,21 @@ public class DemoController {
     }
 
     @GetMapping("/restricted")
-    @PreAuthorize("hasAuthority('Admin')")
+    @PreAuthorize("hasAnyAuthority('Admin','ROLE_ADMIN')")
     public ResponseEntity<String> restricted(){
         return new ResponseEntity<>("You found the secret lair!", HttpStatus.OK);
+    }
+
+    @GetMapping("/api/messages")
+    @PreAuthorize("hasAnyAuthority('Admin','ROLE_ADMIN')")
+    public Map<String, Object> messages() {
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("messages", Arrays.asList(
+                new Message("I am a robot."),
+                new Message("Hello, world!")
+        ));
+
+        return result;
     }
 }
